a800bbca74c28e136a977f1f5896a7b54908406c
Matthew O'Donnell <matthew.l.odonnell@duke.edu>
2024-12-14 01:16:07 -0500

    Added Tables

---
+++ Weighted_Regression.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(rstanarm)
+
+names = read_csv("data/givenname.csv")
+
+# valence models
+summary(lm(name.valence ~ name.gender, weights = names$ppm.1930_1959, data = names))
+summary(lm(name.valence ~ name.gender, weights = names$ppm.1960_1969, data = names))
+summary(lm(name.valence ~ name.gender, weights = names$ppm.1970_1979, data = names))
+summary(lm(name.valence ~ name.gender, weights = names$ppm.1980_1989, data = names))
+summary(lm(name.valence ~ name.gender, weights = names$ppm.1990_1999, data = names))
+summary(lm(name.valence ~ name.gender, weights = names$ppm.2000_2008, data = names))
+
+# warmth models
+summary(lm(name.warmth ~ name.gender, weights = names$ppm.1930_1959, data = names))
+summary(lm(name.warmth ~ name.gender, weights = names$ppm.1960_1969, data = names))
+summary(lm(name.warmth ~ name.gender, weights = names$ppm.1970_1979, data = names))
+summary(lm(name.warmth ~ name.gender, weights = names$ppm.1980_1989, data = names))
+summary(lm(name.warmth ~ name.gender, weights = names$ppm.1990_1999, data = names))
+summary(lm(name.warmth ~ name.gender, weights = names$ppm.2000_2008, data = names))
+
+# competence models
+summary(lm(name.competence ~ name.gender, weights = names$ppm.1930_1959, data = names))
+summary(lm(name.competence ~ name.gender, weights = names$ppm.1960_1969, data = names))
+summary(lm(name.competence ~ name.gender, weights = names$ppm.1970_1979, data = names))
+summary(lm(name.competence ~ name.gender, weights = names$ppm.1980_1989, data = names))
+summary(lm(name.competence ~ name.gender, weights = names$ppm.1990_1999, data = names))
+summary(lm(name.competence ~ name.gender, weights = names$ppm.2000_2008, data = names))
